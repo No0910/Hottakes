644ca4ee785e4ba96ab96e529c3d091396b83537
No0910 <nestea_93@hotmail.fr>
2022-12-12 16:34:22 +0100

    Problème de connexion port 3000/4200

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,6 @@ const userRoutes = require('./routes/user');
 // J'importe le nouveau routeur crée pour les sauces
 //const sauceRoutes = require('./routes/sauce');
 
-
-
 // Je relie mon api à mongoDB
 mongoose.connect('mongodb+srv://nestea_93:Rc1YCEEAMCVcS0Wt@atlascluster.kswowam.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -19,9 +17,6 @@ mongoose.connect('mongodb+srv://nestea_93:Rc1YCEEAMCVcS0Wt@atlascluster.kswowam.
 // Je crée une constante pour l'application express
 const app = express();  
 
-// Je crée un middleware pour analyser le corps de la requête (idem que bodyparser)
-app.use(express.json());
-
 // Je crée un middleware Header pour la sécurité CORS: C'est le 1er middleware qui sera exécuté par notre serveur = middleware général
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,10 +25,12 @@ app.use((req, res, next) => {
   next();
 });
 
+// Je crée un middleware pour analyser le corps de la requête (idem que bodyparser)
+app.use(express.json());
+
 // Routes
 app.use('/api/auth', userRoutes);
 //app.use('/api/sauce', sauceRoutes);
-
 
 // J'exporte cette constante pour pouvoir y accèder depuis les autres fichiers de notre projet (Notamment notre server node)
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,53 @@
-// On importe le package http de Node
+// J'importe le package http de Node
 const http = require('http');
 
-// On importe notre application app.js
+// J'importe notre application app.js
 const app = require('./app');
 
-// On crée la fonction normalizePort qui renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+// Je crée la fonction normalizePort qui renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
 const normalizePort = val => {
-    const port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 };
 
 // Stockage des variables environnement
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 
-// On précise à l'application express sur quel port elle doit tourner :
+// Je précise à l'application express sur quel port elle doit tourner :
 app.set('port', port);
 
-// On crée la fonction errorHandler qui va rechercher et gérer les différents type d'erreurs possible de manière appropriée
-
+// Je crée la fonction errorHandler qui va rechercher et gérer les différents type d'erreurs possible de manière appropriée
 const errorHandler = error => {
-    if (error.syscall !== 'listen') {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
+  }
+};
 
-
-// On crée notre serveur et on lui passe en argument l'application
+// Je crée le serveur et je lui passe en argument notre application
 const server = http.createServer(app);
 
-// On crée un écouteur d'évènements qui consigne le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+// Je crée un écouteur d'évènements qui consigne le port ou le canal nommé sur lequel le serveur s'exécute dans la console
 
 server.on('error', errorHandler);
 server.on('listening', () => {
